12d052ca82cb187cfe53c77845f09c7761975db1
SebV77170 <sebastienvoillot@gmail.com>
2023-03-21 13:05:38 +0000

    Ajout bootstrap dans balise <head> et <script>

--- includes/footer.php
+++ includes/footer.php
@@ -3,3 +3,4 @@
     <p class="paraph_pied_page"> BDD v2.0 Décembre 2022, réalisé par Sébastien VOILLOT pour Ressource'Brie</p>
     
 </footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
