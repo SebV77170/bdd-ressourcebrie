3be0c6fda09538b67ddd80a650da23de8be027d2
SebV77170 <sebastienvoillot@gmail.com>
2024-03-18 10:56:19 +0100

    Modification de getTicket pour la definition de $lien si une facture existe déjà.

--- actions/objets/getTicket.php
+++ actions/objets/getTicket.php
@@ -5,7 +5,15 @@
 $getTicket = $db->prepare('SELECT lien FROM ticketdecaisse WHERE id_ticket = ?');
 $getTicket->execute(array($_GET['id_ticket']));
 
+$getFacture = $db->prepare('SELECT lien FROM facture WHERE id_ticket = ?');
+$getFacture->execute(array($_GET['id_ticket']));
+
 $ticket = $getTicket->fetch();
-$lien = $ticket[0];
+$facture = $getFacture->fetch();
+if(!empty($facture[0])):
+    $lien = $facture[0];
+else:
+    $lien = $ticket[0];
+endif;
 
 ?>
