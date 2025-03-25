1d199c39440930cbdeed5937f50cc550d2a2d9c6
Seb Ordi Bureau <sebastienvoillot@gmail.com>
2025-03-25 14:16:33 +0100

    modif du fichier essai.php

--- essai.php
+++ essai.php
@@ -1,24 +1,7 @@
-<?php
+<!DOCTYPE HTML>
+<html lang="fr-FR">
+<head>
+   
+</body>
+</html>
 
-require 'app/bootstrap.php';
-
-dd($_SERVER);
-
-
-$explode1=explode('?',$_SERVER['REQUEST_URI']);
-$explode2=explode('&', $explode1[1]);
-$explode3=explode('=', $explode2[0]);
-$newPrix = $explode3[1]-5;
-$implode3=implode('=',[$explode3[0],$newPrix]);
-$implode2=implode('&', [$implode3,$explode2[1],$explode2[2],$explode2[3],$explode2[4]]);
-$implode1=implode('?',[$explode1[0],$implode2]);
-$newURL = ''.$implode1.'&ra=true';
-
-?>
-
-<a href=<?=$newURL?> class="stdbouton">valider avec reduction</a>
-
-
-
-
-?>
