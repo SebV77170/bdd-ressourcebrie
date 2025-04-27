f5d04af738647691dd7ddea77d8cdc60d860c4d6
Sebastien Voillot <sebastienvoillot@gmail.com>
2025-04-27 21:59:39 +0200

    Mise en place d'electron.

---
+++ frontend/electron-app/main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+    }
+  });
+
+  win.loadURL('http://localhost:3000'); // Ton React sert sur localhost:3000
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
