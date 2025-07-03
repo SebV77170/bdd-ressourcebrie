2586b8a618589eedcf54da7a5e0ddfbb85ea3d6f
SebV77170 <124190423+SebV77170@users.noreply.github.com>
2025-07-03 23:40:52 +0200

    modification db.php pour inclure mode dev et prod

--- actions/db.php
+++ actions/db.php
@@ -1,16 +1,26 @@
 <?php
-            $dbname = "objets";
-            $serveur = "localhost";
-            $login = "root";
-            $pass = "";
-            
-            try{
-                        $db = new PDO("mysql:host=$serveur;dbname=$dbname;charset=utf8;", $login, $pass);
-                        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            }
-            
-            catch(Exception $e){
-                        die('Une erreur a été trouvée : '.$e->getMessage());
-            }
-            
+
+// Détection automatique de l'environnement
+$host = $_SERVER['HTTP_HOST'];
+
+if ($host === 'bdd' || $host === '127.0.0.1') {
+    // Mode développement
+    $dbname = "objets";
+    $serveur = "localhost";
+    $login = "root";
+    $pass = "";
+} else {
+    // Mode production (à adapter selon ton hébergement)
+    $dbname = "09007_ressourceb";
+    $serveur = "sql01.ouvaton.coop";
+    $login = "09007_ressourceb";
+    $pass = "LaRessourcerieDeBrie77170!";
+}
+
+try {
+    $db = new PDO("mysql:host=$serveur;dbname=$dbname;charset=utf8", $login, $pass);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(Exception $e) {
+    die('Une erreur a été trouvée : '.$e->getMessage());
+}
 ?>
